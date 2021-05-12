53439635d01ac20864c1bfe2f88d2783b1b39ec1
Lario <osunkunleolanrewaju@gmail.com>
2021-05-12 21:48:34 +0100

    added the fronted structure of the application

---
+++ client/src/components/Options.js
@@ -0,0 +1,12 @@
+import React, { Children } from 'react'
+
+const Options = ({children}) => {
+    return (
+        <div>
+            Options
+            {Children}
+        </div>
+    )
+}
+
+export default Options
